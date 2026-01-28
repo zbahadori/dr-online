8c5bf90818014f21188450738bb22fe8de095547
zbahadori <bahadorizahra2017@gmail.com>
2026-01-28 14:00:57 +0330

    [ADD] prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,6 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
         <div className={styles.ctas}>
           <a
             className={styles.primary}
