e91f0765f64e53916615de4077ac12587f2661a8
zbahadori <bahadorizahra2017@gmail.com>
2026-02-04 12:32:47 +0330

    [REMOVE] un necessary files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 // بارگذاری فونت از فایل لوکال
 const vazirmatn = localFont({
-  src: "./fonts/Vazirmatn-Light.ttf",
+  src: "./fonts/Vazirmatn-Regular.ttf",
   display: "swap",
   variable: "--font-vazir", // نام متغیر برای استفاده در CSS
   weight: "100 900",
@@ -15,19 +15,13 @@ export const metadata: Metadata = {
   description: "پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین",
 };
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fa" dir="rtl">
       <body className={`${vazirmatn.variable}`}>{children}</body>
     </html>
   );
