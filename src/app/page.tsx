e855974843fd229ad6642bda9d358504d1b571cb
zbahadori <bahadorizahra2017@gmail.com>
2026-02-04 13:26:01 +0330

    [ADD] dynamic page example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div>سلام رفیق!</div>;
+type Post = {
+  id: number;
+  title: string;
+};
+export default async function Home() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+  const posts: Post[] = await res.json();
+
+  return (
+    <>
+      <div>سلام رفیق!</div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
