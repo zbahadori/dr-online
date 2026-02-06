8429e2bc6efbe38b9ce6bbecc3734549499e439e
zbahadori <bahadorizahra2017@gmail.com>
2026-02-06 13:06:35 +0330

    [Remove] un necessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,7 @@
-type Post = {
-  id: number;
-  title: string;
-};
-
 export default async function Home() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    cache: "no-store",
-  });
-
-  const posts: Post[] = await res.json();
-
   return (
     <>
       <div>سلام رفیق!</div>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
     </>
   );
 }
