89d743807cffacfe2e84cf4e6d5b7fd102e55b44
zbahadori <bahadorizahra2017@gmail.com>
2026-02-07 19:55:10 +0330

    [ADD] error page [UPDATE] global style

---
+++ src/app/error.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ReactElement } from "react";
+
+import Image from "next/image";
+
+import errorImage from "@/assets/illustrations/error.svg";
+
+import styles from "./error.module.css";
+
+type Props = {
+  error: Error & { digest?: string };
+  reset: () => void;
+};
+
+export default function Error({ error, reset }: Props): ReactElement {
+  return (
+    <div className={styles["error"]}>
+      <div className={styles.writings}>
+        <div className={styles["status-code"]}>بدبخت شدیم!</div>
+        <h1>یک خطای غیرمنتظره رخ داده است.</h1>
+        <p>با عرض پوزش، لطفاً با تیم پشتیبانی تماس بگیرید.</p>
+      </div>
+      <div className={styles.visuals}>
+        <Image src={errorImage} alt="" />
+      </div>
+      <div className={styles.actions}>
+        <button onClick={reset}>تلاش مجدد</button>
+      </div>
+      <div className={styles.trace}>
+        <details>
+          <summary>لاگ خطا</summary>
+          <pre dir="ltr">{error.stack}</pre>
+        </details>
+      </div>
+    </div>
+  );
+}
