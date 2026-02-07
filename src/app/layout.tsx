c93222ab0413a188f8e11b7297963b06d6ea5230
zbahadori <bahadorizahra2017@gmail.com>
2026-02-07 18:41:30 +0330

    [UPDATE] style [ADD] typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Vazirmatn } from "next/font/google";
 import HeaderComponent from "@/components/header/header.component";
 import FooterComponent from "@/components/footer/footer.component";
 
+import "@/styles/typography.css";
+
 import "./globals.css";
 
 // بارگذاری فونت از فایل لوکال
